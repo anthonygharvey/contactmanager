02c26ff68c4eb67688d821b5bfa0c8fa83c39ffe
anthonygharvey <anthony@anthonygharvey.com>
2019-04-23 22:02:10 -0400

    Added Contacts component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Contact from "./components/Contact";
+import Contacts from "./components/Contacts";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,12 +10,7 @@ function App() {
     <div className="App">
       <Header branding="Contact Manager" />
       <div className="container">
-        <Contact name="John Doe" email="jdoe@gmail.com" phone="555-555-5555" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <Contacts />
       </div>
     </div>
   );
---
+++ src/components/Contacts.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+
+export class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Karen Williams",
+        email: "karen@gmail.com",
+        phone: "333-333-333"
+      },
+      {
+        id: 3,
+        name: "Henry Johnson",
+        email: "henry@gmail.com",
+        phone: "444-444-44444"
+      }
+    ]
+  };
+
+  render() {
+    const contacts = this.state.contacts.map(contact => {
+      return <Contact key={contact.id} contact={contact} />;
+    });
+
+    return <div>{contacts}</div>;
+  }
+}
+
+export default Contacts;
